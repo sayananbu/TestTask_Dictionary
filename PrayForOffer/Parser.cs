fa8c3723ab788a90019396b1813b073fb883cc91
sayananbu <67857676+sayananbu@users.noreply.github.com>
2023-03-02 00:05:25 +0300

    Add files via upload

---
+++ PrayForOffer/Parser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrayForOffer
+{
+    public static class Parser
+    {
+        static private bool CompareChars(string str1, string str2)
+        {
+            if (str1.Length != str2.Length) 
+                return false;
+            for (int k = 0; k < str1.Length; k++)
+            {
+                if (str1.Contains(str2[k], StringComparison.OrdinalIgnoreCase))
+                {
+                    if (k + 1 == str2.Length) return true;
+                }
+                else break;
+            }
+            return false;
+        }
+        public static string[][] ParseStringArray(string[] arr)
+        {
+            if (arr == null || arr.Length < 1) return new string[][] { };
+            var dict = new Dictionary<string, List<string>>();
+
+            foreach (var item in arr)
+            {
+                if(dict.Count < 1)
+                {
+                    dict.Add(item, new List<string> { item });
+                    continue;
+                }
+                foreach (var key in dict)
+                {
+                    if (CompareChars(key.Key, item))
+                    {
+                        dict[key.Key].Add(item);
+                        break;
+                    }
+                    if(key.Key == dict.Last().Key)
+                    {
+                        dict.Add(item,new List<string> { item});
+                        break;
+                    }
+                }
+            }
+            return dict.Values.Select(x => x.ToArray()).ToArray();
+        }
+    }
+}
---
+++ PrayForOffer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+namespace PrayForOffer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] defaultArray = new string[] { "ток", "рост", "кот", "торс", "Кто", "фывап", "рок" };
+            string[] defaultArray2 = new string[] { "кит","кот", "тор", "раз", "зар","рза"};
+            string[] defaultArray3 = new string[] {};
+            string[] defaultArray4 = new string[] {"5"};
+            var defaultArray5 = new string[] { "floor", "roof", "rofo", "roller", "guide", "idegu" };
+
+            PrintArray(Parser.ParseStringArray(defaultArray));
+            PrintArray(Parser.ParseStringArray(defaultArray2));
+            PrintArray(Parser.ParseStringArray(defaultArray3));
+            PrintArray(Parser.ParseStringArray(defaultArray4));
+            PrintArray(Parser.ParseStringArray(defaultArray5));
+        }
+        static void PrintArray(string[][] array) {
+            foreach (var item in array)
+            {
+                foreach (var el in item)
+                {
+                    Console.Write(el+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
